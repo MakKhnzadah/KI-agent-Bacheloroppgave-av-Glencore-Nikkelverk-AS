72b3ba70f05494458edb893e72eb9914cfb2ee18
Ermin Zec <166255204+erminzec@users.noreply.github.com>
2026-03-03 20:33:17 +0100

    Remove venv from repository and add to gitignore

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
-from .routers import health, documents
+
+from .routers import documents, health
+
 app = FastAPI()
 
 app.include_router(health.router)
 app.include_router(documents.router)
+
